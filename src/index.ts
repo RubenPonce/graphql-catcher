c365d72da3eb7d3d0792e35041d2406a9377cea4
rubenponce <rubenmartinponce@gmail.com>
2023-08-21 14:56:38 -0400

    configure CORS

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,50 @@
 import mongoose from "mongoose";
-import AWS from "aws-sdk";
 import {readFileSync} from "fs";
+import express from 'express';
+import cors from 'cors';
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import {expressMiddleware} from "@apollo/server/express4";
+import {ApolloServerPluginDrainHttpServer} from '@apollo/server/plugin/drainHttpServer';
+import http from "http";
+import * as pkg from "body-parser"
+
 import dotenv from "dotenv";
 import {resolvers} from "./resolvers/resolver";
 import {InMemoryLRUCache} from "apollo-server-caching";
+
 dotenv.config();
-//@TODO enable cors and authorization
-//var express = require("express");
-//var cors = require("cors");
-//var app = express();
-//var corsOptions = {
-//  origin: "<insert uri of front-end domain>",
-//  credentials: true
-//};
-//app.use(cors(corsOptions));
 
+const app = express();
+const httpServer = http.createServer(app);
+const {json} = pkg;
 
-let secret = process.env.secretdb;
-if (!secret && process.env.NODE_ENV === 'production') {
-  const ssmClient = new AWS.SSM({
-    region: 'us-east-1'
-  });
-  ssmClient.getParameter({
-    Name: `/node/secretdb`,
-    WithDecryption: true,
-  }, (err, data) => {
-    if (data?.Parameter) {
-      secret = data.Parameter.Value;
-    }
-  });
-}
+const secret = process.env.secretdb;
 mongoose.connect(`${secret}`).then(() => {
-  console.log("MongoDB connected successfully");
+    console.log("MongoDB connected successfully");
 }).catch((err) => {
-  console.log("unable to connect to MongoDB")
-  console.log(err);
-})
-//@TODO integrate these types with typescript
+    console.log("unable to connect to MongoDB");
+    console.log(err);
+});
+
 const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' });
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  cache: new InMemoryLRUCache()
+    typeDefs,
+    resolvers,
+    cache: new InMemoryLRUCache(),
+    plugins: [ApolloServerPluginDrainHttpServer({httpServer})]
 });
 
-async function startApolloServer(server) {
-  const {url} = await startStandaloneServer(server, {
-    listen: {port: 4000},
-  });
+await server.start();
 
-  console.log(`🚀  Server ready at: ${url}`);
-}
+app.use(express.json());
+app.use('/graphql', expressMiddleware(server));
 
-startApolloServer(server);
+
+// Enable CORS for your domain
+app.use(cors({
+    origin: ["https://catcher.tv", "http://localhost:4000", "https://studio.apollographql.com", "http://localhost:3000"]
+}));
+
+await new Promise<void>((resolve) => httpServer.listen({port: 4000}, resolve));
+console.log(`🚀  Server ready at http://localhost:4000/graphql`);
